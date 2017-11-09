7bd7bb3b65ecce68435c0d5fb8b282f3345c6b1f
Your Name <you@example.com>
2017-11-09 09:52:38 +0100

    kjgljglhgkhg

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>on utilise Git</h1>
+	<div class="contenairer">
+		<div class="row">
+			<?php echo'nous sommes le '.
+			date('d-m-y'); ?>
+			<img src="elandy.jpg">
+			vvdgjejjjyeyueuu
+			joijfjkjfjjjfjjj
+		</div>
+	</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
